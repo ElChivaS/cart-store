b72d3a59e193f591bd15a89e1113f870600658a2
Hermann Caraballo <hermann.caraballo@gmail.com>
2023-10-18 20:29:01 -0400

    initial commit. + integrate menu + create views.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <header class="top-bar spread">
+    <nav class="top-bar-nav">
+      <router-link to="/" class="top-bar-link nav-item">
+          <i class="icofont-spoon-and-fork"></i>
+          <span>Home</span>
+      </router-link>
+      <router-link to="/products" class="top-bar-link nav-item">
+          <span>Products</span>
+      </router-link>
+      <router-link to="/orders" class="top-bar-link nav-item">
+          <span>Past Orders</span>
+      </router-link>
+    </nav>
+<!--    <a @click="toggleSidebar" class="top-bar-cart-link">
+      <i class="icofont-cart-alt icofont-1x"></i>
+      <span>Cart ({{ totalQuantity }})</span>
+    </a>-->
+  </header>
   <router-view/>
 </template>
 
@@ -18,10 +32,10 @@
 nav {
   padding: 30px;
 
-  a {
+  .nav-item {
     font-weight: bold;
     color: #2c3e50;
-
+    padding-right: 9px;
     &.router-link-exact-active {
       color: #42b983;
     }
